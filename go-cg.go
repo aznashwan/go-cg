1b6c3933b5b72328c899147dc762c1f929d55eab
aznashwan <aznashwan@yahoo.com>
2015-02-22 00:25:06 +0200

    Added base package file

---
+++ go-cg.go
@@ -0,0 +1,7 @@
+// All code herein is GPLv2, see LICENSE for details.
+//
+// Author: Azhari Mouhammad-Nashwan
+//
+// This package serves as a central repository for all my Computer-Graphics
+// tasks, as part of my activity at the Polytechnic University of Timisoara
+package cg
